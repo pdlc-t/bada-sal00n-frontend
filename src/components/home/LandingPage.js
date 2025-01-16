e8f22ad3f2f210d9f2e672084c599ac2c40fd827
artur <artur.brogowicz@gmail.com>
2025-01-16 16:59:01 +0100

    hot fix 1 przycisk do logowania

--- src/components/home/LandingPage.js
+++ src/components/home/LandingPage.js
@@ -4,33 +4,26 @@ import NavBar from './NavBar'; // Importuj nowy NavBar
 import '../../css/LandingPage.css';
 
 const LandingPage = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleAdminLogin = () => {
-    navigate('/login');
-  };
+    const handleLogin = () => {
+        navigate('/login');
+    };
 
-  const handleClientLogin = () => {
-    navigate('/login');
-  };
-
-  return (
-    <div className="landing-page">
-      <NavBar /> {/* Dodajemy NavBar na górze strony */}
-      <main className="main-content">
-        <h1>Witaj w naszej Sal00nie!</h1>
-        <button className="login-button" onClick={handleAdminLogin}>
-          Zaloguj się jako admin
-        </button>
-        <button className="login-button" onClick={handleClientLogin}>
-          Zaloguj się jako klient
-        </button>
-      </main>
-      <footer className="footer">
-        <p>©BADA Big Data Development</p>
-      </footer>
-    </div>
-  );
+    return (
+        <div className="landing-page">
+            <NavBar /> {/* Dodajemy NavBar na górze strony */}
+            <main className="main-content">
+                <h1>Witaj w naszej Sal00nie!</h1>
+                <button className="login-button" onClick={handleLogin}>
+                    Zaloguj się do Sal00nu
+                </button>
+            </main>
+            <footer className="footer">
+                <p>©BADA Big Data Development</p>
+            </footer>
+        </div>
+    );
 };
 
 export default LandingPage;
